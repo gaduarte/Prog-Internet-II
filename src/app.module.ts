d4f5b633c27118116f19c264a5eb580ed0f68c9f
gabiduartte <gabiduarte723@gmail.com>
2023-09-14 18:37:14 -0300

    pessoas-1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosModule } from './produtos/produtos.module';
+import { PessoaModule } from './pessoas/pessoas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+//import ormconfig from '/ormconfig.json';
 
 @Module({
-  imports: [ProdutosModule],
+  imports: [
+    PessoaModule,, /*
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: '',
+      password: '',
+      database: '',
+      synchronize: true,
+      logging: true,*/
+      // entities: ['dist/**/*.entity{.ts,.js}'],
+    //}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
